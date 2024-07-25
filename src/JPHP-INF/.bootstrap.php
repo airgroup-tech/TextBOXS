2094d516a2312b017282e1c30b792088ec25092e
airgroup-tech <stikeworld1@mail.ru>
2024-07-25 21:15:57 +0700

    Add sourse code

---
+++ src/JPHP-INF/.bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+// Generated.
+
+use php\gui\framework\Application;
+
+
+
+$app = new Application();
+
+$app->loadModules(array (
+  0 => 'textboxs\modules\AppModule',
+));
+$app->addStyle('/.theme/style.fx.css');
+$app->launch();
+
